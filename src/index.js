7f0a2573797c581f9c4fd942c4c240765e4ba396
Rich Greenhill <rgnet1@yahoo.com>
2016-04-06 12:53:51 -0700

    successfully render first component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // Create new component. This component to produce HTML
 
-
+const App = function() {
+  return <div>Hi!</div>;
+}
 
 // Take this component's generated HTML and put on page.
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+
