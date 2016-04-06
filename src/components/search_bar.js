ebb5f938fcadc6cd1b9e3f7081c805831a3022f4
Rich Greenhill <rgnet1@yahoo.com>
2016-04-06 13:38:32 -0700

    setState and showing state change

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,9 +6,20 @@ import React, { Component } from 'react';
 // All class-based components must have a render function
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { term: '' };
+  }
+  
   render() {
     return(
-      <input onChange={ event => console.log(event.target.value) } />
+      <div>
+        <input onChange={ event => this.setState( { term: event.target.value } ) } />
+        <p>
+          Value of the input: { this.state.term }
+        </p>
+      </div>
     );
   }
   
