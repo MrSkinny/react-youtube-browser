f3a534f949cd67b38e34671dda265ca573e908ad
Rich Greenhill <richdefies@gmail.com>
2016-04-07 18:51:59 +0000

    add function into onClick on li

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
-  // const video = props.video  --> provided by ES6 ({video}) above
+const VideoListItem = ({video, onVideoSelect}) => {
+  // const video = props.video  
+  // const onVideoSelect = props.onVideoSelect
+  // --> provided by ES6 ({video, onVideoSelect}) above
+  
   const imageUrl = video.snippet.thumbnails.default.url;
   
   return (
-    <li className="list-group-item">
+    <li onClick={ () => onVideoSelect(video) } className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageUrl} />
