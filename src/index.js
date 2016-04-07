af38faef9fcfde93a918171ec4a37a218931ca10
Rich Greenhill <rgnet1@yahoo.com>
2016-04-06 17:19:08 -0700

    youtube pull success

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBVgqyNxuUQb3qdmRmbaTSqE0QQu7TFA4Y';
+YTSearch({key: API_KEY, term: 'xbox'}, function(data){
+  console.log(data);
+});
 
 // Create new component. This component to produce HTML
 
