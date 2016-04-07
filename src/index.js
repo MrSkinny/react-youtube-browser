be59539924232bc79668402475f4661ec63d004d
Rich Greenhill <rgnet1@yahoo.com>
2016-04-06 17:25:08 -0700

    set yt data to state

--- src/index.js
+++ src/index.js
@@ -5,17 +5,16 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBVgqyNxuUQb3qdmRmbaTSqE0QQu7TFA4Y';
-YTSearch({key: API_KEY, term: 'xbox'}, function(data){
-  console.log(data);
-});
-
-// Create new component. This component to produce HTML
 
 class App extends Component {
   constructor(props){
     super(props);
     
     this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'xbox' }, (videos) => {
+      this.setState({ videos });
+    });
   }
   
   render() {
